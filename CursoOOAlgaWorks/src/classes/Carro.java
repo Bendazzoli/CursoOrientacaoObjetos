ff1b9cfbfd3097ad4dc812a368c9bb6a30493df9
Bendazzoli <pauloh.bendazzoli@gmail.com>
2017-04-05 14:52:33 -0300

    criando classe Carro

---
+++ CursoOOAlgaWorks/src/classes/Carro.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Carro {
+	
+	//Atributos, Propriedades, Caracteristicas ou ainda Variáveis de Instância
+	String fabricante;
+	String modelo;
+	String cor;
+	String tipoCombustivel;
+	int    anoDeFabricacao;
+	double valorDeMercado;
+}
